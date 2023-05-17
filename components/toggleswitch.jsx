213ad5ea6aabac47386d1c013b4f0a403e73c89d
zackdesu <zarchxxx@gmail.com>
2023-05-17 19:46:21 +0700

    Using icons switch (light, dark) instead using toggle switch

--- components/toggleswitch.jsx
+++ components/toggleswitch.jsx
@@ -1,19 +1,21 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import "../public/css/style.css";
+import { CiLight, CiDark } from "react-icons/ci";
 
 const ToggleSwitch = () => {
   const [isOn, setIsOn] = useState(false);
-
+  const [icon, setIcon] = useState(<CiLight size={35} />);
   const handleIsOn = () => {
     setIsOn(!isOn);
     if (isOn) {
       document.documentElement.classList.remove("dark");
       localStorage.removeItem("theme");
+      setIcon(<CiLight size={35} />);
     } else {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
+      setIcon(<CiDark size={35} />);
     }
   };
 
@@ -23,14 +25,21 @@ const ToggleSwitch = () => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
       setIsOn(true);
+      setIcon(<CiDark size={35} />);
     }
-  }, [isOn, setIsOn]);
+  }, [isOn, setIsOn, icon]);
 
   return (
     <>
-      <input type="checkbox" id="switch" onChange={handleIsOn} checked={isOn} />
+      <input
+        type="checkbox"
+        className="absolute hidden"
+        id="switch"
+        onChange={handleIsOn}
+        checked={isOn}
+      />
       <label htmlFor="switch" className="md:-mr-10">
-        Toggle
+        {icon}
       </label>
     </>
   );
