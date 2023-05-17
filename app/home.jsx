680e9711dfddcba44acd2fc35f093e927a056a96
zackdesu <zarchxxx@gmail.com>
2023-05-17 19:15:09 +0700

    var className, del double div, responsive max-w, responsive icons

--- app/home.jsx
+++ app/home.jsx
@@ -16,41 +16,42 @@ const Home = () => {
   const firstTime = new Date("2022-12-23").getTime();
   const months = Math.floor((now - firstTime) / (1000 * 60 * 60 * 24 * 30));
 
+  const className =
+    "shadow-[0_0_5px_1px] shadow-neutral-800 bg-neutral-200 hover:bg-neutral-400 dark:shadow-neutral-400 dark:bg-neutral-800 rounded-full mx-3 my-4 dark:hover:bg-neutral-600 hover:shadow-cyan-600 hover:dark:shadow-cyan-300";
+
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center h-[85vh] mt-2">
-        <p className="font-FiraCode tracking-widest text-lg mb-2">
-          LET&apos;S START
-        </p>
-        <h1 className="font-Josefin">
-          Hello! I&apos;m{" "}
-          <span className="text-cyan-600 dark:text-cyan-300">Zack</span>
-        </h1>
-        <h1 className="font-Josefin">A Front-end Web Developer</h1>
-        <p className="max-w-xl xl:max-w-4xl mt-2 mx-8 text-center">
-          I'm a Front-end Web Developer. I'm {years} years old. I just started
-          programming for {months} months ago. Currently, I'm learning{" "}
-          {whatDoYouLearnNow}, and doing this project.
-        </p>
-        <ul className="flex my-3">
-          <li className="shadow-[0_0_5px_1px] shadow-neutral-800 bg-neutral-200 hover:bg-neutral-400 dark:shadow-neutral-400 dark:bg-neutral-800 p-3 rounded-full mx-3 my-4 dark:hover:bg-neutral-600">
-            <Link href="https://github.com/zackdesu" target={"_blank"}>
-              <FiGithub size={22} />
-            </Link>
-          </li>
-          <li className="shadow-[0_0_5px_1px] shadow-neutral-800 bg-neutral-200 hover:bg-neutral-400 dark:shadow-neutral-400 dark:bg-neutral-800 p-3 rounded-full mx-3 my-4 dark:hover:bg-neutral-600">
-            <FiFacebook size={22} />
-          </li>
-          <li className="shadow-[0_0_5px_1px] shadow-neutral-800 bg-neutral-200 hover:bg-neutral-400 dark:shadow-neutral-400 dark:bg-neutral-800 p-3 rounded-full mx-3 my-4 dark:hover:bg-neutral-600">
-            <BsDiscord size={22} />
-          </li>
-          <li className="shadow-[0_0_5px_1px] shadow-neutral-800 bg-neutral-200 hover:bg-neutral-400 dark:shadow-neutral-400 dark:bg-neutral-800 p-3 rounded-full mx-3 my-4 dark:hover:bg-neutral-600">
-            <Link href="https://instagram.com/zackdesu" target={"_blank"}>
-              <FiInstagram size={22} />
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <div className="flex flex-col items-center justify-center h-[85vh] my-2">
+      <p className="font-FiraCode tracking-widest text-lg mb-2">
+        LET&apos;S START
+      </p>
+      <h1 className="font-Josefin">
+        Hello! I&apos;m{" "}
+        <span className="text-cyan-600 dark:text-cyan-300">Zack</span>
+      </h1>
+      <h1 className="font-Josefin">A Front-end Web Developer</h1>
+      <p className="max-w-xl xl:max-w-2xl mt-2 mx-8 text-center">
+        I'm a Front-end Web Developer. I'm {years} years old. I just started
+        programming for {months} months ago. Currently, I'm learning{" "}
+        {whatDoYouLearnNow}, and doing this project.
+      </p>
+      <ul className="flex my-3">
+        <li className={className}>
+          <Link href="https://github.com/zackdesu" target={"_blank"}>
+            <FiGithub className="text-[22px] m-3 lg:text-[30px]" />
+          </Link>
+        </li>
+        <li className={className}>
+          <FiFacebook className="text-[22px] m-3 lg:text-[30px]" />
+        </li>
+        <li className={className}>
+          <BsDiscord className="text-[22px] m-3 lg:text-[30px]" />
+        </li>
+        <li className={className}>
+          <Link href="https://instagram.com/zackdesu" target={"_blank"}>
+            <FiInstagram className="text-[22px] m-3 lg:text-[30px]" />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
