29ad3fcca8fad00a78df7e2b3b10975838c6d925
zackdesu <zarchxxx@gmail.com>
2023-05-18 09:11:59 +0700

    List to function, add underline hover

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import ToggleSwitch from "./toggleswitch";
 
+const ListLink = ({ title, pos }) => (
+  <li className="text-slate-800 mx-2 md:mx-3 dark:text-slate-200 font-semibold relative group">
+    {title}
+    <span
+      className={`h-[1px] inline-block w-0 bg-black dark:bg-white absolute right-${pos} -bottom-0.5 lg:group-hover:w-1/2 transition-[width] duration-200`}
+    >
+      &nbsp;
+    </span>
+    <span
+      className={`h-[1px] inline-block w-0 bg-black dark:bg-white absolute left-${pos} -bottom-0.5 lg:group-hover:w-1/2 transition-[width] duration-200`}
+    >
+      &nbsp;
+    </span>
+  </li>
+);
+
 export default function Navbar() {
-  const className =
-    "text-slate-800 mx-2 md:mx-3 dark:text-slate-200 hover:text-cyan-600 hover:dark:text-cyan-300 font-semibold active:text-cyan-600 active:dark:text-cyan-300";
   return (
     <div className="backdrop-blur border-b-2 border-zinc-200 dark:border-zinc-800 text-slate-950 w-full flex items-center justify-around py-5 dark:text-slate-50 sticky top-0">
       <h2 className="md:-ml-10 cursor-pointer hover:text-cyan-600 hover:dark:text-cyan-300">
-        Portfolio
+        zackdesu
       </h2>
       <ul className="flex items-center md:-ml-24 sm:-ml-12">
-        <li className={className}>Home</li>
-        <li className={className}>About</li>
-        <li className={className}>Skills</li>
-        <li className={className}>Project</li>
+        <ListLink title="Home" pos={5} />
+        <ListLink title="About" pos={5} />
+        <ListLink title="Project" pos={6} />
+        <ListLink title="Contact" pos={6} />
       </ul>
       <ToggleSwitch />
     </div>
