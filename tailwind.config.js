e96ec7e96c79f0e73549b0f25b5d591c8df865b6
zackdesu <zarchxxx@gmail.com>
2023-05-19 18:57:27 +0700

    Add Font Lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
       fontFamily: {
         FiraCode: ["Fira Code", "monospace"],
         Josefin: ["Josefin Sans", "sans-serif"],
+        Lato: ["Lato", "sans-serif"],
       },
     },
   },
