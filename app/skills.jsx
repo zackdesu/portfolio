4932ce4cbc5f009f1758e59f73dc39bfffd8de73
zackdesu <zarchxxx@gmail.com>
2023-05-20 20:18:09 +0700

    Updating skills / tech stack

--- app/skills.jsx
+++ app/skills.jsx
@@ -1,19 +1,24 @@
-import Image from "next/image";
 import React from "react";
+import Icons from "@/components/Icons";
 
 const Skills = () => {
-  const ListItem = ({ img, name }) => (
-    <li className="cursor-default flex justify-around items-center w-3/4 bg-zinc-300 mx-auto my-3 rounded-xl p-5 font-semibold text-xl">
-      <Image src={img} width={50} height={50} alt={name} />
-      {name}
-    </li>
-  );
-
   return (
-    <div className="h-[90vh] p-5" id="skills">
-      <h1 className="text-center">My Skills</h1>
-      <ul>
-        <ListItem img={"/img/html.png"} name={"HTML"} />
+    <div
+      className="flex flex-col justify-center items-center my-24 h-52"
+      id="skills"
+    >
+      <h1 className="font-Josefin">Tech Stacks</h1>
+      <ul className="flex my-3 w-4/6 lg:w-3/6 xl:w-2/6 mx-auto justify-center flex-wrap">
+        <Icons url={"/img/html.png"} name="Hypertext Markup Language" />
+        <Icons url={"/img/css.png"} name="Cascading Style Sheets" />
+        <Icons url={"/img/js.png"} name="JavaScript" />
+        <Icons url={"/img/react.png"} name="React.js" />
+        <Icons url={"/img/next.svg"} name="Next.js" />
+        <Icons url={"/img/tailwind.png"} name="Tailwind CSS" />
+        <Icons url={"/img/express.png"} name="Express.js" />
+        <Icons url={"/img/canva.png"} name="Canva" />
+        <Icons url={"/img/figma.png"} name="Figma" />
+        <Icons url={"/img/mongodb.png"} name="MongoDB" />
       </ul>
     </div>
   );
