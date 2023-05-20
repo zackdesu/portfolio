d91a267c7fc10af4e20326632b70e310755ed18f
zackdesu <zarchxxx@gmail.com>
2023-05-20 16:03:44 +0700

    formatting

--- app/about.jsx
+++ app/about.jsx
@@ -9,27 +9,23 @@ const About = () => {
   const years = Math.floor(months / 12);
 
   return (
-    <div
-      className="
-    flex justify-center flex-col lg:flex-row
-    h-[90vh] md:items-center bg-neutral-200 dark:bg-neutral-950
-    
-    "
-      id="about"
-    >
-      <div>
-        <Image
-          src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-          alt="images"
-          width={400}
-          height={400}
-          className="border-2 border-zinc-300 bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-800 p-5 mx-auto lg:mx-10 lg:w-[450px] xl:w-[500px] 2xl:w-[700px]"
-        />
-      </div>
-      <div className="mx-10 flex my-5 flex-col lg:text-end font-Lato">
-        <h1 className="mx-auto mb-3 lg:m-0 font-[700] xl:text-5xl">About Me</h1>
-        {/* prettier-ignore */}
-        <p className="mx-auto lg:mx-0 max-w-xl mt-2 font-[300] xl:text-lg">
+    <div className="bg-neutral-200 dark:bg-neutral-950 p-5" id="about">
+      <div className="flex justify-center flex-col lg:flex-row md:items-center">
+        <div className="relative">
+          <Image
+            src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            alt="images"
+            width={400}
+            height={400}
+            className="border-2 border-zinc-300 bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-800 p-5 mx-auto lg:mx-10 lg:w-[450px] xl:w-[500px] 2xl:w-[700px] rounded-xl hover:xl:scale-75 hover:2xl:scale-105 duration-200 xl:transition-all shadow-[0_0_5px_.5px] shadow-neutral-800 dark:shadow-neutral-400 xl:hover:shadow-none"
+          />
+        </div>
+        <div className="mx-10 flex my-5 flex-col lg:text-end font-Lato">
+          <h1 className="mx-auto mb-3 lg:m-0 font-[700] xl:text-5xl">
+            Who I am
+          </h1>
+          {/* prettier-ignore */}
+          <p className="mx-auto lg:mx-0 max-w-xl mt-2 font-[300] xl:text-lg">
           Hello, my name is Zack. a front-end web developer with{" "}
           {months < 13
             ? `${months} months of experience.`
@@ -51,6 +47,7 @@ const About = () => {
           and build robust back-end systems. With my knowledge of Canva and
           Figma, I can create design that enhance the overall user interface.
         </p>
+        </div>
       </div>
     </div>
   );
