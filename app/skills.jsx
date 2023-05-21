9d21495e7ee6b3e80661c48b632e858320a8bab6
zackdesu <zarchxxx@gmail.com>
2023-05-21 12:43:32 +0700

    replace img file & update src img

--- app/skills.jsx
+++ app/skills.jsx
@@ -9,16 +9,16 @@ const Skills = () => {
     >
       <h1 className="font-Josefin">Tech Stacks</h1>
       <ul className="flex my-3 w-4/6 lg:w-3/6 xl:w-2/6 mx-auto justify-center flex-wrap">
-        <Icons url={"/img/html.png"} name="Hypertext Markup Language" />
-        <Icons url={"/img/css.png"} name="Cascading Style Sheets" />
-        <Icons url={"/img/js.png"} name="JavaScript" />
-        <Icons url={"/img/react.png"} name="React.js" />
-        <Icons url={"/img/next.svg"} name="Next.js" />
-        <Icons url={"/img/tailwind.png"} name="Tailwind CSS" />
-        <Icons url={"/img/express.png"} name="Express.js" />
-        <Icons url={"/img/canva.png"} name="Canva" />
-        <Icons url={"/img/figma.png"} name="Figma" />
-        <Icons url={"/img/mongodb.png"} name="MongoDB" />
+        <Icons url={"/img/logo/html.png"} name="Hypertext Markup Language" />
+        <Icons url={"/img/logo/css.png"} name="Cascading Style Sheets" />
+        <Icons url={"/img/logo/js.png"} name="JavaScript" />
+        <Icons url={"/img/logo/react.png"} name="React.js" />
+        <Icons url={"/img/logo/next.svg"} name="Next.js" />
+        <Icons url={"/img/logo/tailwind.png"} name="Tailwind CSS" />
+        <Icons url={"/img/logo/express.png"} name="Express.js" />
+        <Icons url={"/img/logo/canva.png"} name="Canva" />
+        <Icons url={"/img/logo/figma.png"} name="Figma" />
+        <Icons url={"/img/logo/mongodb.png"} name="MongoDB" />
       </ul>
     </div>
   );
