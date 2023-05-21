3930a72c2264f5b084b745318e913a46d2a514ff
zackdesu <zarchxxx@gmail.com>
2023-05-21 09:07:58 +0700

    image hover about scale fix

--- app/about.jsx
+++ app/about.jsx
@@ -17,7 +17,7 @@ const About = () => {
             alt="images"
             width={400}
             height={400}
-            className="border-2 border-zinc-300 bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-800 p-5 mx-auto lg:mx-10 lg:w-[450px] xl:w-[500px] 2xl:w-[700px] rounded-xl hover:xl:scale-75 hover:2xl:scale-105 duration-200 xl:transition-all shadow-[0_0_5px_.5px] shadow-neutral-800 dark:shadow-neutral-400 xl:hover:shadow-none"
+            className="border-2 border-zinc-300 bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-800 p-5 mx-auto lg:mx-10 lg:w-[450px] xl:w-[500px] 2xl:w-[700px] rounded-xl hover:xl:scale-105 duration-200 xl:transition-all shadow-[0_0_5px_.5px] shadow-neutral-800 dark:shadow-neutral-400 xl:hover:shadow-none"
           />
         </div>
         <div className="mx-10 flex my-5 flex-col text-center lg:text-end font-Lato">
