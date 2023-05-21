8e110f8e823c38c0f7d8434501ead352d596bbdf
zackdesu <zarchxxx@gmail.com>
2023-05-21 12:42:39 +0700

    add my projects

---
+++ app/myprojects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MyProjects = () => {
+  return (
+    <div>
+      <h1 className="text-center">Projects</h1>
+      <ul className="flex justify-center">
+        <li className="flex justify-center bg-slate-300 p-5 my-5 w-4/12">
+          Calculator
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MyProjects;
