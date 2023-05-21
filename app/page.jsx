66d63747a709a6c1ae45619ceddadf65ca8b7839
zackdesu <zarchxxx@gmail.com>
2023-05-21 12:42:49 +0700

    add my projects

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./home";
 import About from "./about";
 import Skills from "./skills";
+import MyProjects from "./myprojects";
 
 const Page = () => (
   <>
     <Home />
     <About />
     <Skills />
+    <MyProjects />
   </>
 );
 
