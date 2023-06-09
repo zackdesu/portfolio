dca03fd9f95bb26567d0a8594d2134f53ae93217
zackdesu <zarchxxx@gmail.com>
2023-06-09 20:16:59 +0700

    Deleting contact

--- pages/api/component/apiRateLimiter.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-const apiRateLimiter = rateLimit({
-  windowMs: 1000 * 60 * 60,
-  max: 3,
-  message: { message: "Form submission limit reached" },
-});
-
-export default apiRateLimiter;
