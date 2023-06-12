64ff460c2cf63d3cdf114ed89f391710869e8bf4
zackdesu <zarchxxx@gmail.com>
2023-06-12 22:10:03 +0700

    tech stack adjust responsive

--- app/skills.jsx
+++ app/skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
       id="skills"
     >
       <h1 className="font-Josefin">Tech Stacks</h1>
-      <ul className="flex my-3 w-4/6 lg:w-3/6 xl:w-2/6 mx-auto justify-center flex-wrap">
+      <ul className="flex my-3 w-4/6 sm:w-[455px] lg:w-[505px] lg:h-[188px] mx-auto justify-center flex-wrap">
         <Icons url={"/img/logo/html.png"} name="Hypertext Markup Language" />
         <Icons url={"/img/logo/css.png"} name="Cascading Style Sheets" />
         <Icons url={"/img/logo/js.png"} name="JavaScript" />
